da9b21bd72b2a3e7f171bdefe9638af3b1f89e0b
oryaeer <oryaeeramor111@gmail.com>
2023-06-07 14:17:19 +0300

    d

--- instances_ctl_2.sh
+++ instances_ctl_2.sh
@@ -48,5 +48,26 @@ case $action in
     echo "Invalid action. Please specify --stop, --start, or --destroy."
     exit 1
     ;;
+  
+  --createami)
+  
+
+  # Launch EC2 instance
+  echo "Launching EC2 instance..."
+  instance_id=$(aws ec2 run-instances --image-id "ami-0715c1897453cabd1" --instance-type "t2.micro" --key-name "oryaeer" --subnet-id "subnet-099271a2111dd07db" --security-group-ids "sg-0a24d62166110b86e" --count "1" --output text --query 'Instances[0].InstanceId')
+  echo "Instance launched with ID: $instance_id"
+
+  # Wait for instance to be running
+  echo "Waiting for the instance to be running..."
+  aws ec2 wait instance-running --instance-ids $instance_id
+  echo "Instance is now running"
+  
+  sudo git clone https://github.com/oryaeer/flask.git /home/ec2-user
+  aws ec2 create-image --instance-id $instance_id --name "MyAMI" --description "My custom AMI"
+  echo "created ami"
+  ;;
+  
+
+
 esac
 
